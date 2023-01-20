1a5cb47d190be243dd00eb92db6b30e03586c7f0
blueberry <121964338+prettyblueberry@users.noreply.github.com>
2023-01-20 13:56:29 +0900

    cancel action

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,4 +1,8 @@
-function ProjectForm() {
+interface ProjectFormProps {
+    onCancel: () => void;
+}
+
+function ProjectForm({ onCancel }: ProjectFormProps) {
     return (
         <form className="input-group vertical">
             <label htmlFor="name">Project Name</label>
@@ -12,7 +16,8 @@ function ProjectForm() {
             <div className="input-group">
                 <button className="primary bordered medium">Save</button>
                 <span />
-                <button type="button" className="bordered medium">
+                <button type="button" className="bordered medium"
+                onClick={onCancel}>
                     cancel
                 </button>
             </div>
