27749cb956d46775f301f2fead79d7ce56992c93
blueberry <121964338+prettyblueberry@users.noreply.github.com>
2023-01-21 17:43:45 +0900

    backend data: show "loading...", "error!"

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,12 @@
-import React, { Fragment, useState } from 'react';
-import { MOCK_PROJECTS } from './MockProjects';
+import React, { Fragment, useState, useEffect } from 'react';
 import ProjectList from './ProjectList';
 import { Project } from './Project';
+import { projectAPI } from './projectAPI';
 
 function ProjectsPage() {
-    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | undefined>(undefined);
 
     const saveProject = (project: Project) => {
         let updatedProjects = projects.map((p: Project) => {
@@ -13,11 +15,70 @@ function ProjectsPage() {
         setProjects(updatedProjects);
       };
 
+    // Approach 1: using promise then
+//  useEffect(() => {
+//    setLoading(true);
+//    projectAPI
+//      .get(1)
+//      .then((data) => {
+//        setError(null);
+//        setLoading(false);
+//        setProjects(data);
+//      })
+//      .catch((e) => {
+//        setLoading(false);
+//        setError(e.message);
+//        if (e instanceof Error) {
+//           setError(e.message);
+//        }
+//      });
+//  }, []);
+
+    // Approach 2: using async/await
+      useEffect(() => {
+            async function loadProjects() {
+                  setLoading(true);
+                  try {
+                        const data = await projectAPI.get(1);
+                        setError('');
+                        setProjects(data);
+                      }
+                   catch (e) {
+                        if (e instanceof Error) {
+                              setError(e.message);
+                            }
+                        } finally {
+                        setLoading(false);
+                      }
+                }
+            loadProjects();
+          }, []);
+
     return (
         <Fragment>
             <h1>Projects</h1>
+
+                  {error && (
+                    <div className="row">
+                          <div className="card large error">
+                            <section>
+                              <p>
+                                <span className="icon-alert inverse "></span>
+                                {error}
+                              </p>
+                            </section>
+                          </div>
+                        </div>
+                  )}
+
             <ProjectList
                      onSave={saveProject} projects={projects} />
+                  {loading && (
+                    <div className="center-page">
+                          <span className="spinner primary"></span>
+                          <p>Loading...</p>
+                        </div>
+                  )}
         </Fragment>
     );
 }
