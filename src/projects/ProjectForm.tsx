15041dbefd247f59486f67867b1ff72c8dbb33df
blueberry <121964338+prettyblueberry@users.noreply.github.com>
2023-01-21 17:00:06 +0900

    create backend server

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -9,8 +9,15 @@ interface ProjectFormProps {
 
 function ProjectForm({ project: initialProject, onSave, onCancel }: ProjectFormProps) {
     const [project, setProject] = useState(initialProject);
+    const [errors, setErrors] = useState({
+        name: '',
+        description: '',
+        budget: '',
+    });
+
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
+        if(!isValid()) return;
         onSave(project);
     };
 
@@ -29,27 +36,65 @@ function ProjectForm({ project: initialProject, onSave, onCancel }: ProjectFormP
                 };
 
                 let updatedProject: Project;
-            // need to do functional update b/c
+                // need to do functional update b/c
                 // the new project state is based on the previous project state
-                    // so we can keep the project properties that aren't being edited +like project.id
-                        // the spread operator (...) is used to
-                            // spread the previous project properties and the new change
-                                setProject((p) => {
-                                      updatedProject = new Project({ ...p, ...change });
-                                      return updatedProject;
-                                    });
+                // so we can keep the project properties that aren't being edited +like project.id
+                // the spread operator (...) is used to
+                // spread the previous project properties and the new change
+                setProject((p) => {
+                      updatedProject = new Project({ ...p, ...change });
+                      return updatedProject;
+                    });
+                setErrors(() => validate(updatedProject));
           };
+      function validate(project: Project) {
+            let errors: any = { name: '', description: '', budget: '' };
+            if (project.name.length === 0) {
+                  errors.name = 'Name is required';
+                }
+            if (project.name.length > 0 && project.name.length < 3) {
+                  errors.name = 'Name needs to be at least 3 characters.';
+                }
+            if (project.description.length === 0) {
+                  errors.description = 'Description is required.';
+                }
+            if (project.budget === 0) {
+                  errors.budget = 'Budget must be more than $0.';
+                }
+            return errors;
+          }
 
+      function isValid() {
+            return (
+                  errors.name.length === 0 &&
+                  errors.description.length === 0 &&
+                  errors.budget.length === 0
+                );
+          }
     return (
         <form className="input-group vertical"
             onSubmit={handleSubmit}
         >
             <label htmlFor="name">Project Name</label>
+            {errors.name.length > 0 && (
+                <div className="card error">
+                    <p>{errors.name}</p>
+                </div>
+            )}
             <input type="text" name="name" placeholder="enter name" value={project.name} onChange={handleChange}/>
             <label htmlFor="description">Project Description</label>
             <textarea name="description" placeholder="enter description" value={project.description} onChange={handleChange}/>
-            <label htmlFor="budget">Project Budget</label>
+                  {errors.description.length > 0 && (
+                    <div className="card error">
+                          <p>{errors.description}</p>
+                        </div>
+                  )}<label htmlFor="budget">Project Budget</label>
             <input type="number" name="budget" placeholder="enter budget" value={project.budget} onChange={handleChange}/>
+                  {errors.budget.length > 0 && (
+                    <div className="card error">
+                          <p>{errors.budget}</p>
+                        </div>
+                  )}
             <label htmlFor="isActive">Active?</label>
             <input type="checkbox" name="isActive"  checked={project.isActive} onChange={handleChange}/>
             <div className="input-group">
